76421ebc753e5991260c6be0c3fa778936c41c1c
luizedua42 <luizedua@student.42sp.org.br>
2024-02-01 15:08:17 -0300

    Contacts getters and setters

--- cpp00/ex01/srcs/Contacts.cpp
+++ cpp00/ex01/srcs/Contacts.cpp
@@ -17,18 +17,36 @@ Contacts::~Contacts(){}
 void Contacts::setFirstName(std::string firstName) {
 	this->_firstName = firstName;
 }
+
+std::string Contacts::getFirstName(){
+	return this->_firstName;
+}
+
 void Contacts::setLastName(std::string lastName) {
 	this->_lastName = lastName;
+}
+std::string Contacts::getLastName(){
+	return this->_lastName;
 }
 void Contacts::setNickName(std::string nickName) {
 	this->_nickName = nickName;
 }
-void Contacts::setFirstName(std::string firstName) {
-	this->_firstName = firstName;
+std::string Contacts::getNickName(){
+	return this->_nickName;
 }
-void Contacts::setFirstName(std::string firstName) {
-	this->_firstName = firstName;
+
+void Contacts::setPhoneNumber(std::string phoneNumber) {
+	this->_phoneNumber = phoneNumber;
 }
-void Contacts::setFirstName(std::string firstName) {
-	this->_firstName = firstName;
+std::string Contacts::getPhoneNumber(){
+	return this->_phoneNumber ;
 }
+
+void Contacts::setDarkestSecret(std::string darkestSecret) {
+	this->_darkestSecret = darkestSecret;
+}
+std::string Contacts::getDarkestSecret(){
+	return this->_darkestSecret;
+}
+
+
