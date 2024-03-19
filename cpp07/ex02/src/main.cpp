c916ed89b63ceb3c03afdc6555381040a5775bae
luizedua42 <luizedua@student.42sp.org.br>
2024-03-19 14:43:43 -0300

    ccp7/2 intra main

--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -1,32 +1,53 @@
-/**======================
-*            42sp
-* @file      : main.cpp
-* @author    : luizedua
-* @email     : luizedua@student.42sp.org.br
-* @createdOn : 13/03/2024
-*========================**/
-
+#include <iostream>
 #include "../include/Array.hpp"
 
-int main(void) {
-	//test array template with int, float and char types
+#define MAX_VAL 750
+int main(int, char**)
+{
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
 
-	Array<int> intArray(5);
-	Array<float> floatArray(5);
-	Array<char> charArray(5);
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-	std::cout << "intArray size: " << intArray.size() << std::endl;
-	std::cout << "floatArray size: " << floatArray.size() << std::endl;
-	std::cout << "charArray size: " << charArray.size() << std::endl;
-	std::cout << "copy tests ====================" << std::endl;
-
-	Array<int> intArray2(intArray);
-	Array<float> floatArray2(floatArray);
-	Array<char> charArray2(charArray);
-
-	std::cout << "intArray2 size: " << intArray2.size() << std::endl;
-	std::cout << "floatArray2 size: " << floatArray2.size() << std::endl;
-	std::cout << "charArray2 size: " << charArray2.size() << std::endl;
-
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] mirror;//
 	return 0;
 }
