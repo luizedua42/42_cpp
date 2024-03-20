25bc165f0c69088505fc490a3a83a4e8f9e61dcd
luizedua42 <luizedua@student.42sp.org.br>
2024-03-20 16:16:43 -0300

    starting 02

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -9,15 +9,22 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 # include <iostream>
+# include <vector>
+# include <deque>
 
 class PmergeMe {
 	private:
-
+		std::vector<int> _intV;
+		std::deque<int> _intD;
 	public:
 		PmergeMe(void);
 		PmergeMe(const PmergeMe& rhs);
 		~PmergeMe(void);
 		PmergeMe& operator=(const PmergeMe& rhs);
+		void processInput(char **argv);
+		void validateInput(void);
+		void sortDeque(void);
+		void sortVector(void);
 };
 
 # endif
--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -7,3 +7,58 @@
 *========================**/
 
 #include "../include/PmergeMe.hpp"
+#include <sstream>
+
+PmergeMe::PmergeMe() : _intD(), _intV(){};
+
+PmergeMe::PmergeMe(const PmergeMe& rhs){
+	*this = rhs;
+}
+
+PmergeMe::~PmergeMe(void){}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& rhs) {
+	if(this != &rhs) {
+		_intD = rhs._intD;
+		_intV = rhs._intV;
+	}
+	return *this;
+}
+
+void PmergeMe::validateInput(void) {
+	for (size_t i = 0; i < _intV.size(); i++) {
+		for (size_t j = i + 1; j < _intV.size(); j++) {
+			if (_intV[i] == _intV[j]) {
+				std::cout << "Error: duplicated input" << std::endl;
+				exit(1);
+			}
+		}
+	}
+}
+
+void PmergeMe::processInput(char **argv) {
+	std::istringstream iss;
+	int num = 0;
+
+	iss.exceptions(std::ios::failbit);
+	for (size_t i = 1; argv[i]; i++) {
+		iss.str(argv[i]);
+		try{
+			iss >> num;
+		} catch (std::exception &e) {
+			std::cout << "Error: invalid input: it must be int" << std::endl;
+			exit(1);
+		}
+		if (num < 1) {
+			std::cout << "Error: invalid input: it must be greater than 0" << std::endl;
+			exit(1);
+		}
+		_intV.push_back(num); 
+		iss.clear();
+	}
+	PmergeMe::validateInput();
+}
+
+void PmergeMe::sortDeque(void) {
+
+}
